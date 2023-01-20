a25e88ceb8ff6f475053c66cfe2a20feee8d1dd2
ludovic19 <l.ludovic19@gmail.com>
2023-01-20 11:58:23 +0100

    1er require node

---
+++ ironhackers.js
@@ -0,0 +1,6 @@
+const chalk = require("chalk");
+console.log(chalk.blue("hello Yacine"));
+console.log(chalk.yellow("hello Agash"));
+console.log(chalk.red("hello Vaï"));
+console.log(chalk.green("hello Hicham"));
+console.log(chalk.orange("hello Dimitri"));
